d7be67165c1b7dd49dff08b874e2cc051d9fccc6
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-16 10:43:53 -0300

    Fragmentos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Frase from "./components/Frase";
 import Pessoa from "./components/Pessoa";
 import SayMyName from "./components/SayMyName";
-
 function App() {
   const nome = "Maria";
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+
+      <Frase />
+      <Frase />
       <SayMyName nome="Jucelio" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
